9a32fc45dedb217eedf371f4e1ec36bd9332ab66
Petru Marian Burlacu <40285539@live.napier.ac.uk>
2022-04-16 21:38:19 +0100

    feat: add tailwind, ng-zorro configuration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NzTableModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
