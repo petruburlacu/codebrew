dd7faf19f69b00b3791a1abc494513812582a2fe
Petru Marian Burlacu <40285539@live.napier.ac.uk>
2022-04-21 19:35:37 +0100

    feat: add scroll to top button

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   toggleMobile: boolean = false;
+
+  myButton: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.myButton = document.getElementById("btn-back-to-top");
+    window.addEventListener('scroll', this.scroll, true);
   }
 
   onNavbarToggle(): void {
     this.toggleMobile = !this.toggleMobile;
+  }
+
+  scroll = (event: any): void => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      this.myButton.style.display = "block";
+    } else {
+      this.myButton.style.display = "none";
+    }
+  }
+
+  scrollToTop(): void {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
   }
 
 }
