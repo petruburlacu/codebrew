6c6f84190df9ac72b197f913d6b26dc016a7ed6e
Petru Marian Burlacu <40285539@live.napier.ac.uk>
2022-04-25 22:57:12 +0100

    feat: add Strapi API integration

--- client/src/app/pages/blog/data-access/blog.service.ts
+++ client/src/app/pages/blog/data-access/blog.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Article } from '../models/article';
 import { BlogPost } from '../models/blog-post';
 
 @Injectable({
@@ -8,11 +9,20 @@ import { BlogPost } from '../models/blog-post';
 })
 export class BlogService {
 
-  private _blogPosts = 'blog-posts';
+  private _blogPosts = 'api/blog-posts';
+  private _articles = 'api/articles';
 
   constructor(private http: HttpClient) { }
 
   getBlogPosts() {
     return this.http.get<BlogPost[]>(environment.apiUrl + this._blogPosts);
+  }
+
+  getArticles() {
+    return this.http.get<Article[]>(environment.apiUrl + this._articles);
+  }
+
+  getArticle() {
+    return this.http.get<Article>(environment.apiUrl + this._articles + '/1');
   }
 }
